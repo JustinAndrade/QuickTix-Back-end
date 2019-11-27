6095e07a0c4fa4fd79b815645a567a282e127479
JustinAndrade <you@example.com>
2019-11-26 20:41:07 -0500

    user route created

--- server/server.js
+++ server/server.js
@@ -4,10 +4,13 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const users = require('./routes/users')
+
 server.use(express.json());
 server.use(morgan('dev'));
 server.use(cors());
 server.use(helmet());
+server.use('/users', users)
 
 server.get('/', (req, res) => {
     res.json({ message: 'Welcome to the QuickTix Backend' })
